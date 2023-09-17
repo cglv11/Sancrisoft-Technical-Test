95eadd68c06bace26b672b761c0d32424f5feea9
Camilo Galvis <camilo@dsinno.io>
2023-09-17 23:40:06 +0200

    finish almost all tasks

--- frontend/src/components/EditModal/EditVehicleModal.js
+++ frontend/src/components/EditModal/EditVehicleModal.js
@@ -1,11 +1,19 @@
 import React, { useState, useContext } from 'react';
 import { Modal, TextField, Button } from '@material-ui/core';
 import { VehicleContext } from '../../context/VehicleContext';
+import { CircularProgress } from '@material-ui/core';
+import { Snackbar } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+
 import './EditVehicleModal.css';
 
 function EditVehicleModal({ isOpen, onClose, vehicleData }) {
     const [vehicle, setVehicle] = useState(vehicleData);
     const { updateVehicle } = useContext(VehicleContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('success');
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -13,26 +21,85 @@ function EditVehicleModal({ isOpen, onClose, vehicleData }) {
     };
 
     const handleSubmit = async () => {
-        // Call the updateVehicle function from context
-        const updated = await updateVehicle(vehicle);
-        if (updated) {
-            onClose();
+        setIsLoading(true);
+    
+        try {
+            const updated = await updateVehicle(vehicle);
+    
+            if (updated) {
+                setAlertSeverity('success');
+                setSnackbarMessage('Vehicle updated successfully!');
+                setOpenSnackbar(true);
+                onClose();
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            setAlertSeverity('error');
+            setSnackbarMessage('Error updating vehicle. Please try again.');
+            setOpenSnackbar(true);
         }
+    
+        setIsLoading(false);
     };
 
     return (
         <Modal open={isOpen} onClose={onClose}>
             <div className="modalContent">
                 <h2>Edit Vehicle</h2>
+                <div className="fieldsRow">
+                    <TextField
+                        required
+                        label="Year"
+                        name="year"
+                        value={vehicle.year}
+                        onChange={handleInputChange}
+                        className="yearField"
+                    />
+                    <TextField
+                        required
+                        label="Make"
+                        name="make"
+                        value={vehicle.make}
+                        onChange={handleInputChange}
+                        className="makeField"
+                    />
+                </div>
                 <TextField
                     required
-                    label="Year"
-                    name="year"
-                    value={vehicle.year}
+                    label="Model"
+                    name="model"
+                    value={vehicle.model}
                     onChange={handleInputChange}
+                    className="modelField"
                 />
-                {/* Add other fields similarly */}
-                <Button onClick={handleSubmit}>Update</Button>
+                <div className="buttonsRow">
+                    <Button 
+                        onClick={onClose} 
+                        variant="outlined" 
+                        disabled={isLoading}  // Disable when loading
+                    >
+                        Cancel
+                    </Button>
+                    <Button 
+                        onClick={handleSubmit} 
+                        variant="contained" 
+                        color="primary" 
+                        disabled={isLoading}  // Disable when loading
+                    >
+                        {isLoading ? <CircularProgress size={24} /> : "Update"}
+                    </Button>
+                </div>
+                <Snackbar 
+                    open={openSnackbar} 
+                    autoHideDuration={6000} 
+                    onClose={() => setOpenSnackbar(false)}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                >
+                <Alert onClose={() => setOpenSnackbar(false)} severity={alertSeverity}>
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
             </div>
         </Modal>
     );
--- frontend/src/context/VehicleContext.js
+++ frontend/src/context/VehicleContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import axios from 'axios';
 
 export const VehicleContext = createContext();
 
@@ -6,8 +7,27 @@ export const VehicleProvider = ({ children }) => {
     const [vehicles, setVehicles] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    const updateVehicle = async (vehicleData) => {
+        try {
+            const response = await axios.put(`http://localhost:3000/vehicles/${vehicleData.id}`, vehicleData);
+            if (response.status === 200) {
+                // Find the index of the updated vehicle
+                const index = vehicles.findIndex(v => v.id === vehicleData.id);
+                if (index !== -1) {
+                    const newVehicles = [...vehicles];
+                    newVehicles[index] = vehicleData; // Update the vehicle in the array
+                    setVehicles(newVehicles); // Update the state with the new array
+                }
+                return true;
+            }
+        } catch (error) {
+            console.error('Failed to update vehicle:', error);
+            return false;  // Indicate failure
+        }
+    }
+
     return (
-        <VehicleContext.Provider value={{ vehicles, setVehicles, loading, setLoading }}>
+        <VehicleContext.Provider value={{ vehicles, setVehicles, loading, setLoading, updateVehicle }}>
             {children}
         </VehicleContext.Provider>
     );
