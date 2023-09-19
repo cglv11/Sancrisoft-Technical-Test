faa8895172aab5a1e83a82b6cc533271ccbbdf6d
Camilo Galvis <camilo@dsinno.io>
2023-09-19 16:09:16 +0200

    add tasks 1.1 and 1.2

--- backend/src/setupData.js
+++ backend/src/setupData.js
@@ -3,22 +3,45 @@ const axios = require('axios');
 const db = require("./utils/sqlite");
 
 function storeVehicleData(vehicle) {
-    const sql = `
-        INSERT INTO vehicles (city_mpg, class, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-    db.run(sql, [vehicle.city_mpg, vehicle.class, vehicle.combination_mpg, vehicle.cylinders, vehicle.displacement, vehicle.drive, vehicle.fuel_type, vehicle.highway_mpg, vehicle.make, vehicle.model, vehicle.transmission, vehicle.year]);
+    const incrementedYear = vehicle.year + 2;
+
+    return new Promise((resolve, reject) => {
+        const sql = `
+            INSERT INTO vehicles (city_mpg, class, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        db.run(sql, [vehicle.city_mpg, vehicle.class, vehicle.combination_mpg, vehicle.cylinders, vehicle.displacement, vehicle.drive, vehicle.fuel_type, vehicle.highway_mpg, vehicle.make, vehicle.model, vehicle.transmission, incrementedYear], function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
-axios.get('https://api.api-ninjas.com/v1/cars?limit=49&year=2020', {
-    headers: {
-        'X-Api-Key': 'cHHHBVdIjCrDep9ciWgiNg==UDZY9HNxkh3SLXz4'  // Replace with your actual API key
-    }
-})
-.then(response => {
-    const vehicles = response.data;
-    vehicles.forEach(storeVehicleData);
+function fetchAndStoreCars(year) {
+    return axios.get(`https://api.api-ninjas.com/v1/cars?limit=10&year=${year}`, {
+        headers: {
+            'X-Api-Key': 'cHHHBVdIjCrDep9ciWgiNg==UDZY9HNxkh3SLXz4'
+        }
+    })
+    .then(response => {
+        const vehicles = response.data;
+        return Promise.all(vehicles.map(storeVehicleData));
+    });
+}
+
+let promises = [];
+for (let year = 2000; year <= 2020; year++) {
+    promises.push(fetchAndStoreCars(year));
+}
+
+Promise.all(promises)
+.then(() => {
+    console.log("Database is filled with data!");
 })
 .catch(error => {
-    console.error('Request failed:', error);
+    console.error("Error while fetching and storing data:", error);
 });
